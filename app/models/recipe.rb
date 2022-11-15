e4153e9e17bf3e00fbbe110eae102705a774adad
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-15 06:19:50 -0500

    Add indexfil for food

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
     has_many :recipe_food, foreign_key: "recipe_id"
     belongs_to :user, foreign_key: "user_id"
+
+    validates :name, :preparation_time, :desciption, presence:true
+    
 end
