f60d7a019e2356f3d10eba9f371adb77bdd33f4e
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-15 02:01:37 -0500

    Add setup

--- db/migrate/20221114232701_create_foods.rb
+++ db/migrate/20221114232701_create_foods.rb
@@ -3,7 +3,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
     create_table :foods do |t|
       t.string :name
       t.integer :measurement_unit
-      t.integer :price
+      t.decimal :price
       t.integer :quantity
 
       t.timestamps
