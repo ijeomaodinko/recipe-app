ac62b0b6a2963bdb89ec8e085921e4d1386fd4e1
unknown <tyaps002@gmail,com>
2022-11-18 18:32:01 +0100

    Implement shopping list functionality

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,16 +4,19 @@ class RecipeFoodsController < ApplicationController
   # before_action :set_user
   # GET /recipe_foods or /recipe_foods.json
   def index
-    @recipe_foods = RecipeFood.all
+    @recipe_foods = RecipeFood.find_by(@recipe).includes(:food)
   end
 
   # GET /recipe_foods/1 or /recipe_foods/1.json
-  def show; end
+  def show;
+    @recipe_food = RecipeFood.find_by(id: params[:recipe_id])
+  end
 
   # GET /recipe_foods/new
   def new
     @recipe_food = RecipeFood.new
-    @recipe = Recipe.find_by(params[:recipe_id])
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    # @recipe = Recipe.foods.build
   end
 
   # GET /recipe_foods/1/edit
@@ -22,6 +25,7 @@ class RecipeFoodsController < ApplicationController
   # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe_food.recipe = @recipe
 
     respond_to do |format|
       if @recipe_food.save
