08844978cbf6b2940a9a441736ce4db02852073c
ijeomaodinko <ijeomaodinko@gmail.com>
2023-09-11 13:56:15 +0100

    Add files

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,6 @@
-# exit on error
-set -e
-
-# Remove carriage return characters (if any)
-sed -i 's/\r//' ./render-build.sh
-
+set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake db:migrate
 
-#if you have seeds to run add:
-# bundle exec rails db:seed
+bundle exec rake db:seed
